71da6df0c38dfaf0eeb92722302f41e278d29e48
MuzammilSheikh786 <muzammil26122004@gmail.com>
2024-09-24 14:20:00 +0500

    Global and local variable concept

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict";
+// Global Variable
+// let person1 = "sohail";
+// if(true){
+//     console.log(person1)
+// }
+// console.log(person1);
+// Local Variable:
+// if(true){
+//     let person2 ="Shakeel";
+//     console.log(person2);
+// }
+// console.log(person2);
+// Merge example of global and local variable
+let globalVar = "Accessible everywhere";
+function showExample() {
+    let localVar = "Accessible only inside this function";
+    console.log(globalVar);
+}
---
+++ main.ts
@@ -0,0 +1,18 @@
+// Global Variable
+let person1 = "sohail";
+if(true){
+    console.log(person1)
+
+}
+console.log(person1);
+
+// Local Variable:
+if(true){
+    let person2 ="Shakeel";
+    console.log(person2);
+}
+// console.log(person2);      // => person2 is not exist here because person2 is declared inside the if block,making it a local variable that can only be accessed within that block.
+
+
+
+
